f3fb544412d0e8b47890f14596bd4f6857a6a600
Chandra\nPrakash <chandraprakash47637@gmail.com>
2024-07-18 11:58:57 +0530

    Added Forgot Password and Reset Password Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,68 @@
-import React, { useEffect } from "react";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
-import Home from "./components/Home/Home";
-import Stocks from "./components/Stocks/Stocks";
-import Knowledge from "./components/Knowledge/Knowledge";
-import About from "./components/About/About";
-import LoginPage from "./components/Login/Login";
-import RegisterPage from "./components/Register/Register";
-import axios from "axios";
-import { AuthContext } from "./context/AuthContext";
-import { useState } from "react";
-import Cookies from "js-cookie";
+import React, { useEffect } from 'react';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
+import Stocks from './components/Stocks/Stocks';
+import Knowledge from './components/Knowledge/Knowledge';
+import About from './components/About/About';
+import LoginPage from './components/Login/Login';
+import RegisterPage from './components/Register/Register';
+import axios from 'axios';
+import { AuthContext } from './context/AuthContext';
+import { useState } from 'react';
+import Cookies from 'js-cookie';
+import ResetPassword from './components/ResetPassword/ResetPassword';
+import ForgotPassword from './components/ForgotPassword/ForgotPassword';
 
 const App = () => {
-  axios.defaults.withCredentials = true;
+    axios.defaults.withCredentials = true;
 
-  const check_cookie_is_alive = async () => {
-    const expiryDate = new Date(Cookies.get("token"));
-    if (expiryDate < new Date()) {
-      // Token is expired
-      return true;
-    }
-    return false;
-  };
+    const check_cookie_is_alive = async () => {
+        const expiryDate = new Date(Cookies.get('token'));
+        if (expiryDate < new Date()) {
+            // Token is expired
+            return true;
+        }
+        return false;
+    };
 
-  const login = async (email, password) => {
-    const { data } = await axios.post("http://localhost:3000/api/users/login", {
-      email,
-      password,
-    });
-    console.log(data, "app.jsx code");
-  };
+    const login = async (email, password) => {
+        const { data } = await axios.post(
+            'http://localhost:3000/api/users/login',
+            {
+                email,
+                password,
+            }
+        );
+        console.log(data, 'app.jsx code');
+    };
 
-  const logout = () => {
-    axios.post("http://localhost:3000/api/users/logout");
-  };
+    const logout = () => {
+        axios.post('http://localhost:3000/api/users/logout');
+    };
 
-  return (
-    <AuthContext.Provider value={{ login, logout }}>
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/stocks" element={<Stocks />} />
-          <Route path="/knowledge" element={<Knowledge />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-        <Footer />
-      </Router>
-    </AuthContext.Provider>
-  );
+    return (
+        <AuthContext.Provider value={{ login, logout }}>
+            <Router>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/register" element={<RegisterPage />} />
+                    <Route path="/stocks" element={<Stocks />} />
+                    <Route path="/knowledge" element={<Knowledge />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/reset-password" element={<ResetPassword />} />
+                    <Route
+                        path="/forgot-password"
+                        element={<ForgotPassword />}
+                    />
+                </Routes>
+                <Footer />
+            </Router>
+        </AuthContext.Provider>
+    );
 };
 
 export default App;
